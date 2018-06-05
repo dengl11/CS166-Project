64c36800b5be5b2380948df810ef30c3bbacacd7
dengl11 <dengl11@stanford.edu>
2018-06-04 22:22:51 -0700

    -

--- demo/explore.py
+++ demo/explore.py
@@ -2,23 +2,28 @@
 from render import *
 import webbrowser
 
+def bye():
+    print()
+    print("-=-=-=- Bye --=-=-= ")
+    exit() 
 
 print("Welcome to CS166-Levenshtein Automation Walker !")
 print("-------------------------------------------------")
 while 1:
     print("Type your word for Levenshtein, test word and max edit distance like: <Levenshtein_Word> <Test_Word> k")
     print("Your input: ")
-    user_input = input()
+    try:
+        user_input = input()
+    except KeyboardInterrupt:
+        bye()
     if (user_input == "exit"): 
-        print("-=-=-=- Bye --=-=-= ")
-        exit() 
+        bye()
     words = user_input.split()
     if len(words) != 3:
         print("Invalid input >_<")
         continue 
     w, test_w, k = words
     output = render(w, test_w, int(k))
-    print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
     webbrowser.open('file://' + os.path.realpath(output))
     print()
     print()
